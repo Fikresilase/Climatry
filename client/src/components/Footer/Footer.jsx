276f5b456a526985f98a29553da364be09c679db
Fikresilase <shitubilbila@gmail.com>
2024-09-15 02:33:11 +0300

    footer complete

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -6,21 +6,17 @@ const Footer = () => {
       <div className="paddings innerWidth flexCenter f-container">
         {/* left side */}
         <div className="flexColStart f-left">
-          <img src="./logo2.png" alt="" width={120} />
+          {/*<img src="./logo2.png" alt="" width={120} /> */}
+          <h1>climatery</h1>
           <span className="secondaryText">
-            Our vision is to make all people <br />
-            the best place to live for them.
+            for a better world.
           </span>
         </div>
 
         <div className="flexColStart f-right">
-          <span className="primaryText">Information</span>
-          <span className="secondaryText">145 New York, FL 5467, USA</span>
+          <span className="secondaryText">1000 Addis Ababa, Ethiopia</span>
           <div className="flexCenter f-menu">
-            <span>Property</span>
-            <span>Services</span>
-            <span>Product</span>
-            <span>About Us</span>
+            
           </div>
         </div>
       </div>
