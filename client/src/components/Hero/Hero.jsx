85d1bbc1da32b350044ec4777f1005c3a557315f
Fikresilase <shitubilbila@gmail.com>
2024-09-15 11:44:19 +0300

    hero section refubrished

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -4,13 +4,14 @@ import { motion } from "framer-motion";
 import SearchBar from "../SearchBar/SearchBar";
 const Hero = () => {
   return (
-    <section className="hero-wrapper">
-      <div className="paddings innerWidth flexCenter hero-container">
+    <section className=" bg-blackhero-wrapper">
+      <div className="paddings bg-black innerWidth flexCenter hero-container">
         {/* left side */}
         <div className="flexColStart hero-left">
           <div className="hero-title">
             <div className="orange-circle" />
             <motion.h1
+            className="text-white"
             initial={{ y: "2rem", opacity: 0 }}
             animate={{ y: 0, opacity: 1 }}
             transition={{
@@ -31,21 +32,21 @@ const Hero = () => {
           <SearchBar/>
 
           <div className="flexCenter stats">
-            <div className="flexColCenter stat">
+            <div className="flexColCenter text-white stat">
               <span>
                 <CountUp start={8800} end={9000} duration={4} /> <span>+</span>
               </span>
               <span className="secondaryText">Regions to be Monitored</span>
             </div>
 
-            <div className="flexColCenter stat">
+            <div className="flexColCenter text-white stat">
               <span>
                 <CountUp start={1950} end={2000} duration={4} /> <span>+</span>
               </span>
               <span className="secondaryText">Data streams</span>
             </div>
 
-            <div className="flexColCenter stat">
+            <div className="flexColCenter text-white stat">
               <span>
                 <CountUp end={28} /> <span>+</span>
               </span>
